5f56f183d259545558dbd31cd59fc672e53cd10b
George Pyott <george.pyott@googlemail.com>
2021-07-27 21:21:39 +0100

    Create onscroll animations

---
+++ js/on-scroll.js
@@ -0,0 +1,32 @@
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+scrollElements.forEach((el) => {
+    el.style.opacity = 0
+  });
+
+  const elementInView = (el, scrollOffset = 0) => {
+    const elementTop = el.getBoundingClientRect().top;
+   
+    return (
+      elementTop <= 
+      ((window.innerHeight || document.documentElement.clientHeight) - scrollOffset)
+    );
+  };
+
+  const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+  };
+
+  const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+      if (elementInView(el, 100)) {
+        displayScrollElement(el);
+      }
+    })
+  }
+
+  window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+  });
+
+  console.log("file found")
