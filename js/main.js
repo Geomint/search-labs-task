244d8ca9eaed87575fef063393aeba6207082db5
George Pyott <george.pyott@googlemail.com>
2021-07-28 09:49:15 +0100

    Added links to anchor tags

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 //nav open button
-$('.nav-button').on('click', function () {
+$('.nav-button').on('click', function (e) {
     $('html').addClass('open-nav show-overlay');
+    e.preventDefault;
 });
 
 //click to close
-$('.overlay, .mobile-nav').on('click', function () {
+$('.overlay, .mobile-nav .close-nav').on('click', function () {
     $('html').removeClass('open-nav show-overlay')
 });
+
+$('.close-nav').on('click', function () {
+    $('html').removeClass('open-nav show-overlay')
+})
+
